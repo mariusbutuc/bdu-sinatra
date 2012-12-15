9b46e5c9851a2a3cc6190b378ec9b47f44b8b983
Marius Butuc <marius.butuc@gmail.com>
2012-12-14 21:39:48 -0500

    KISS: Keep only Testimonials + seeds
    Let's start only with the model for Testimonials, and seed 276
    testimonials about BigDataUniversity.com

--- models.rb
+++ models.rb
@@ -1,39 +1,39 @@
 DataMapper.setup(:default, "sqlite://#{Dir.pwd}/bdu.db")
 
-module DownloadableResource
-  def self.included base
-    base.class_eval do
-      include DataMapper::Resource
+# module DownloadableResource
+#   def self.included base
+#     base.class_eval do
+#       include DataMapper::Resource
 
-      property :created_at,   DateTime
-      property :modified_at,  DateTime
-      property :active,       base::Boolean,  default: true
+#       property :created_at,   DateTime
+#       property :modified_at,  DateTime
+#       property :active,       base::Boolean,  default: true
 
-      property :position,     Integer
+#       property :position,     Integer
 
-      property :title,        String,   required: true
-      property :url,          base::URI,      required: true
-      property :description,  base::Text,     required: true
+#       property :title,        String,   required: true
+#       property :url,          base::URI,      required: true
+#       property :description,  base::Text,     required: true
 
-      property :type,         base::Discriminator
-    end
-  end
-end
+#       property :type,         base::Discriminator
+#     end
+#   end
+# end
 
-class News
-  include DataMapper::Resource
-  property :id,           Serial
-  property :created_at,   DateTime
-  property :modified_at,  DateTime
-  property :active,       Boolean,  default: true
+# class News
+#   include DataMapper::Resource
+#   property :id,           Serial
+#   property :created_at,   DateTime
+#   property :modified_at,  DateTime
+#   property :active,       Boolean,  default: true
 
-  property :position,     Integer
+#   property :position,     Integer
 
-  property :title,        String,   required: true, length: 100
-  property :description,  Text
-  property :url,          URI,      required: true
-  property :img_url,      URI,      required: true
-end
+#   property :title,        String,   required: true, length: 100
+#   property :description,  Text
+#   property :url,          URI,      required: true
+#   property :img_url,      URI,      required: true
+# end
 
 class Testimonial
   include DataMapper::Resource
@@ -48,32 +48,32 @@ class Testimonial
   property :email,        String
 end
 
-class Book
-  include DownloadableResource
-  property :id,           Serial
+# class Book
+#   include DownloadableResource
+#   property :id,           Serial
 
-  property :cover_url,    URI
-  property :authors,      String,   required: true, length: 255
-end
+#   property :cover_url,    URI
+#   property :authors,      String,   required: true, length: 255
+# end
 
-class Download
-  include DownloadableResource
-  property :id,           Serial
+# class Download
+#   include DownloadableResource
+#   property :id,           Serial
 
-  property :icon_url,     URI
-end
+#   property :icon_url,     URI
+# end
 
-class Faq
-  include DataMapper::Resource
-  property :id,           Serial
-  property :created_at,   DateTime
-  property :modified_at,  DateTime
-  property :active,       Boolean,  default: true
+# class Faq
+#   include DataMapper::Resource
+#   property :id,           Serial
+#   property :created_at,   DateTime
+#   property :modified_at,  DateTime
+#   property :active,       Boolean,  default: true
 
-  property :position,     Integer
+#   property :position,     Integer
 
-  property :question,     String,   required: true, length: 255
-  property :answer,       Text,     required: true
-end
+#   property :question,     String,   required: true, length: 255
+#   property :answer,       Text,     required: true
+# end
 
 DataMapper.auto_upgrade!
